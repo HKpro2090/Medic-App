a9fd9a17b24c9c975cb0b251f2bea420c5dcc4be
HariK <harikrishnan.vamsi@gmail.com>
2021-11-04 20:41:25 +0530

    Login, Signup, changepassword fragment design completed!

--- app/src/main/java/com/example/medic_app/MainActivity.java
+++ app/src/main/java/com/example/medic_app/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.medic_app;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +19,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
        Bundle users = new Bundle();
        users.putString("ajiteshjs2001@gmail.com","");
+       loadFragment(new LoginFragment());
+
+
+
+
+    }
+
+    public void loadFragment(Fragment fragment)
+    {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.RegistrationFrame, fragment);
+        fragmentTransaction.commit();
+    }
+
+    public void loadtoast()
+    {
+        Toast.makeText(getApplicationContext(),"Signing in",Toast.LENGTH_LONG).show();
+        System.out.println("Clicked");
     }
 }
