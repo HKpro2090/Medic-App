e283948ccdb6bb864fc638f54b48312f8dc9fb4f
Ajitesh <ajiteshjs2001@gmail.com>
2021-11-19 21:14:28 +0530

    Forgot Password base structure created

---
+++ app/src/main/java/com/example/medic_app/ResetPasswordFragment.java
@@ -0,0 +1,39 @@
+package com.example.medic_app;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class ResetPasswordFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_reset_password, container, false);
+
+        Button fpassbtn=(Button)view.findViewById(R.id.resetPassEmailButton);
+        fpassbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity)getActivity()).imageresize(0.1f);
+                ((MainActivity)getActivity()).makeimgempty();
+                ((MainActivity)getActivity()).makefragmentbig(0.77f);
+                FragmentManager m=getFragmentManager();
+                FragmentTransaction ft=m.beginTransaction();
+                ft.replace(R.id.RegistrationFrame,new ChangePasswordFragment());
+                ft.commit();
+            }
+        });
+
+        return view;
+    }
+}
