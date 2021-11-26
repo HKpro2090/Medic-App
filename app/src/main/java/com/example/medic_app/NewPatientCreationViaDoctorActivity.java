282a6d607bd2026defbd1bd1eaf1451da11033f1
HariK <harikrishnan.vamsi@gmail.com>
2021-11-26 13:16:20 +0530

    Fixed complete profile fragment and other new fragment created

---
+++ app/src/main/java/com/example/medic_app/NewPatientCreationViaDoctorActivity.java
@@ -0,0 +1,14 @@
+package com.example.medic_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewPatientCreationViaDoctorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_patient_creation_via_doctor);
+    }
+}
