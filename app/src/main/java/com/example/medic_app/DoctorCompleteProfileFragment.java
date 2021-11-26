c9369c3b22b2b6c08fec11b5dc39a89d5d922e78
HariK <harikrishnan.vamsi@gmail.com>
2021-11-26 13:25:51 +0530

    renamed files

--- app/src/main/java/com/example/medic_app/DoctorCompleteProfileFragment.java
+++ app/src/main/java/com/example/medic_app/DoctorCompleteProfileFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link DoctorCompleteProfile#newInstance} factory method to
+ * Use the {@link DoctorCompleteProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DoctorCompleteProfile extends Fragment {
+public class DoctorCompleteProfileFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class DoctorCompleteProfile extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public DoctorCompleteProfile() {
+    public DoctorCompleteProfileFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class DoctorCompleteProfile extends Fragment {
      * @return A new instance of fragment DoctorCompleteProfile.
      */
     // TODO: Rename and change types and number of parameters
-    public static DoctorCompleteProfile newInstance(String param1, String param2) {
-        DoctorCompleteProfile fragment = new DoctorCompleteProfile();
+    public static DoctorCompleteProfileFragment newInstance(String param1, String param2) {
+        DoctorCompleteProfileFragment fragment = new DoctorCompleteProfileFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
