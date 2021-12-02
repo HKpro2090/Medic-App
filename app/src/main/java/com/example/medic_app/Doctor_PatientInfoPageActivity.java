b41c0ab9eeb2d7db73894eda1391533899e2a661
Ajitesh <ajiteshjs2001@gmail.com>
2021-12-02 19:14:46 +0530

    renamed Doctor-patient info page

--- app/src/main/java/com/example/medic_app/Doctor_PatientInfoPageActivity.java
+++ app/src/main/java/com/example/medic_app/Doctor_PatientInfoPageActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class PatientInfoPageActivity extends AppCompatActivity {
+public class Doctor_PatientInfoPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_patient_info_page);
+        setContentView(R.layout.activity_doctor_patient_info_page);
     }
 }
