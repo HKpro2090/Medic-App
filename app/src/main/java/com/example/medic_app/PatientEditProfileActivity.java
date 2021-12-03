a9fba3d652d0b775f4aabe46d31926f41be6dacd
Ajitesh <ajiteshjs2001@gmail.com>
2021-12-03 18:07:02 +0530

    gender, bgrp

--- app/src/main/java/com/example/medic_app/PatientEditProfileActivity.java
+++ app/src/main/java/com/example/medic_app/PatientEditProfileActivity.java
@@ -17,10 +17,13 @@ import android.text.InputType;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -46,8 +49,13 @@ public class PatientEditProfileActivity extends AppCompatActivity {
     int SELECT_PICTURE = 200;
     int patientdp=R.drawable.patient2;
     EditText pname,page,pgen,pbgrp,pheight,pweight,pemail,pphno;
+    Spinner pg,pb;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     CollectionReference user_col = db.collection("users");
+    String[] bloodgroups = { "A+", "A-",
+            "B+", "B-",
+            "O+", "O-","AB-","AB+" };
+    String[] genders = {"Male","Female","Prefer not to Say"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +84,10 @@ public class PatientEditProfileActivity extends AppCompatActivity {
         ImageButton editHeight=(ImageButton)findViewById(R.id.editPheight);
         ImageButton editWeight=(ImageButton)findViewById(R.id.editPWeight);
 
+        pg=findViewById(R.id.PatientEditProfileGenderspinner);
+        pb=findViewById(R.id.PatientEditProfileBgrpspinner);
+
+
         user_col.document("user_"+email).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
@@ -101,6 +113,10 @@ public class PatientEditProfileActivity extends AppCompatActivity {
                         pemail.setText(email);
                         pheight.setText(pht);
                         pweight.setText(pweit);
+                        ArrayAdapter<String> adp=new ArrayAdapter<String>(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,genders);
+                        pg.setAdapter(adp);
+                        ArrayAdapter<String> ad=new ArrayAdapter<String>(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,bloodgroups);
+                        pb.setAdapter(ad);
                         nonEditable();
 
 
@@ -137,15 +153,21 @@ public class PatientEditProfileActivity extends AppCompatActivity {
         editgender.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pgen.setEnabled(true);
-                pgen.setInputType(InputType.TYPE_CLASS_TEXT);
+//                pgen.setEnabled(true);
+//                pgen.setInputType(InputType.TYPE_CLASS_TEXT);
+                pgen.setVisibility(View.GONE);
+                pg.setVisibility(View.VISIBLE);
+                pg.setEnabled(true);
             }
         });
         editbgrp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pbgrp.setEnabled(true);
-                pbgrp.setInputType(InputType.TYPE_CLASS_TEXT);
+//                pbgrp.setEnabled(true);
+//                pbgrp.setInputType(InputType.TYPE_CLASS_TEXT);
+                pbgrp.setVisibility(View.GONE);
+                pb.setVisibility(View.VISIBLE);
+                pb.setEnabled(true);
             }
         });
         editphno.setOnClickListener(new View.OnClickListener() {
@@ -187,6 +209,29 @@ public class PatientEditProfileActivity extends AppCompatActivity {
             }
         });
 
+        pg.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                gender=genders[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        pb.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                bldgrp=bloodgroups[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         FloatingActionButton updt=(FloatingActionButton) findViewById(R.id.editProfileUpdateButton2);
         updt.setOnClickListener(new View.OnClickListener() {
@@ -194,12 +239,13 @@ public class PatientEditProfileActivity extends AppCompatActivity {
             public void onClick(View v) {
                 name=pname.getText().toString();
                 age=page.getText().toString();
-                gender=pgen.getText().toString();
-                bldgrp=pbgrp.getText().toString();
                 phno=pphno.getText().toString();
                 pht=pheight.getText().toString();
                 pweit=pweight.getText().toString();
-
+                pgen.setVisibility(View.VISIBLE);
+                pgen.setText(gender);
+                pbgrp.setVisibility(View.VISIBLE);
+                pbgrp.setText(bldgrp);
                 Map<String,Object> edit_details = new Hashtable<>();
                 edit_details.put("user_name_key",name);
                 edit_details.put("age_key",age);
@@ -234,6 +280,10 @@ public class PatientEditProfileActivity extends AppCompatActivity {
     private void nonEditable(){
         pname.setEnabled(false);
         pname.setInputType(InputType.TYPE_NULL);
+        pg.setVisibility(View.GONE);
+        pg.setEnabled(false);
+        pb.setVisibility(View.GONE);
+        pb.setEnabled(false);
         page.setEnabled(false);
         page.setInputType(InputType.TYPE_NULL);
         pgen.setEnabled(false);
