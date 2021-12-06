0abd9c0cee7be646455570048e7c67ab452ec51c
GAUTAM P A <gautampa7@gmail.com>
2021-12-06 16:34:58 +0530

    View Sessions Page Functionality Completed

--- app/src/main/java/com/example/medic_app/DoctorDailyAppointmentsListFragment.java
+++ app/src/main/java/com/example/medic_app/DoctorDailyAppointmentsListFragment.java
@@ -2,18 +2,36 @@ package com.example.medic_app;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
 
 public class DoctorDailyAppointmentsListFragment extends Fragment {
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference user_col = db.collection("users");
+
+    String[] slots = {"Slot1","Slot2","Slot3","Slot4","Slot5","Slot6","Slot7","Slot8",
+            "Slot9","Slot10","Slot11","Slot12","Slot13","Slot14","Slot15","Slot16"};
+    String[] slot_timings = {"09:00 to 09:30", "09:30 to 10:00","10:00 to 10:30","10:30 to 11:00","11:00 to 11:30",
+            "11:30 to 12:00","12:00 to 12:30","12:30 to 13:00","14:00 to 14:30","14:30 to 15:00","15:00 to 15:30",
+            "15:30 to 16:00", "16:00 to 16:30","16:30 to 17:00","17:00 to 17:30","17:30 to 18:00"};
+    int slot_nos=16;
+    String doctor_email,selected_date="";
     ArrayList<String> appointment_date_slot = new ArrayList<>();
     ArrayList<String> ap_status = new ArrayList<>();
     PatientUpcomingListAdapter lv_adapter;
@@ -24,17 +42,42 @@ public class DoctorDailyAppointmentsListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_doctor_daily_appointments_list, container, false);
-        appointment_date_slot.add("Slot 4 4:30pm");
-        appointment_date_slot.add("Slot 1 9:30pm");
-        appointment_date_slot.add("Slot 2 11:00pm");
-        ap_status.add("Free");
-        ap_status.add("Booked");
-        ap_status.add("Booked");
+
+        doctor_email = getArguments().getString("doc_email");
+        selected_date = getArguments().getString("session_date");
+
         lv=(ListView) view.findViewById(R.id.DoctorDailyAppointments);
         lv_adapter = new PatientUpcomingListAdapter(this, appointment_date_slot, ap_status);
+        Toast.makeText(getContext(),doctor_email+" " + selected_date,Toast.LENGTH_SHORT).show();
+        user_col.document("user_"+doctor_email).collection("Sessions").document(selected_date).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                if(task.isSuccessful()){
+                    DocumentSnapshot document = task.getResult();
+                    for (int i=0;i<slot_nos;i++){
+                        if(document.getString(slots[i])!=null){
+                            appointment_date_slot.add("Slot "+(i+1)+" - "+slot_timings[i]);
+                            ap_status.add(document.getString(slots[i]));
+                        }
+                    }
+
+                    lv_adapter = new PatientUpcomingListAdapter(DoctorDailyAppointmentsListFragment.this, appointment_date_slot, ap_status);
+                    lv_adapter.notifyDataSetChanged();
+                    lv.setAdapter(lv_adapter);
+                }else{
+                    Toast.makeText(getContext(),"FireBase Error!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+//        appointment_date_slot.add("Slot 4 4:30pm");
+//        appointment_date_slot.add("Slot 1 9:30pm");
+//        appointment_date_slot.add("Slot 2 11:00pm");
+//        ap_status.add("Free");
+//        ap_status.add("Booked");
+//        ap_status.add("Booked");
 
         lv.setAdapter(lv_adapter);
-
         return view;
     }
 }
--- app/src/main/java/com/example/medic_app/DoctorSessionsFragment.java
+++ app/src/main/java/com/example/medic_app/DoctorSessionsFragment.java
@@ -1,7 +1,12 @@
 package com.example.medic_app;
 
+import android.content.DialogInterface;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -9,18 +14,131 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class DoctorSessionsFragment extends Fragment {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DoctorSessionsFragment extends Fragment  implements DatePickerDialog.OnDateSetListener{
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference user_col = db.collection("users");
+    String doctor_email,selected_date="";
+    ArrayList<String> existing_session_dates = new ArrayList<String>();
+    int Year, Month, Day;
+    DatePickerDialog datePickerDialog;
+    TextView text_date_picker;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_doctor_sessions, container, false);
-        FragmentManager fm=getFragmentManager();
-        FragmentTransaction ft= fm.beginTransaction();
-        ft.replace(R.id.SessionsPreviewFragmentContainer,new DoctorDailyAppointmentsListFragment());
-        ft.commit();
+
+        text_date_picker = (TextView) view.findViewById(R.id.datePickerView);
+        doctor_email = getArguments().getString("email");
+        ArrayList<Calendar> doc_avail_dates_calendar = new ArrayList<Calendar>();
+
+
+        text_date_picker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user_col.document("user_"+doctor_email).collection("Sessions").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        doc_avail_dates_calendar.clear();
+                        if (task.isSuccessful()) {
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+                            for (DocumentSnapshot document : task.getResult()) {
+                                existing_session_dates.add(document.getId());
+                                Date date = new Date();
+                                try {
+                                    date = sdf.parse(document.getId());
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                                if (date != null) {
+                                    Calendar calendar;
+                                    calendar = Calendar.getInstance();
+                                    calendar.setTime(date);
+                                    doc_avail_dates_calendar.add(calendar);
+                                }
+                            }
+
+                            datePickerDialog = DatePickerDialog.newInstance(DoctorSessionsFragment.this, Year, Month, Day);
+                            int nightModeFlag = getContext().getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+                            switch (nightModeFlag)
+                            {
+                                case Configuration.UI_MODE_NIGHT_YES:
+                                    datePickerDialog.setThemeDark(true);
+                                    break;
+                                case Configuration.UI_MODE_NIGHT_NO:
+                                    datePickerDialog.setThemeDark(false);
+                                    break;
+                                case Configuration.UI_MODE_NIGHT_UNDEFINED:
+                                    datePickerDialog.setThemeDark(false);
+                                    break;
+
+                            }
+
+                            Calendar[] doc_avail_dates_cal_array = new Calendar[doc_avail_dates_calendar.size()];
+                            doc_avail_dates_calendar.toArray(doc_avail_dates_cal_array);
+                            datePickerDialog.setTitle("Select Session Date");
+                            datePickerDialog.setSelectableDays(doc_avail_dates_cal_array);
+                            datePickerDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+
+                                @Override
+                                public void onCancel(DialogInterface dialogInterface) {
+                                    Toast.makeText(getContext(), "Date Picker Canceled", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                            datePickerDialog.show(getChildFragmentManager(), "DatePickerDialog");
+                        }
+                    }
+                });
+            }
+        });
+
         return view;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onDateSet(DatePickerDialog view, int Year, int Month, int Day) {
+
+        String selected_date_textview = Day+"/"+(Month+1)+"/"+Year;
+
+        LocalDate now = LocalDate.of(Year,Month+1,Day);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+        selected_date = dtf.format(now);
+        text_date_picker.setText(selected_date_textview);
+
+        Toast.makeText(getContext(), "Selected Date:"+selected_date, Toast.LENGTH_SHORT).show();
+
+        FragmentManager fm=getChildFragmentManager();
+        FragmentTransaction ft= fm.beginTransaction();
+        Bundle data = new Bundle();
+        data.putString("doc_email",doctor_email);
+        data.putString("session_date",selected_date);
+        Fragment Sessions_List = new DoctorDailyAppointmentsListFragment();
+        Sessions_List.setArguments(data);
+        ft.replace(R.id.SessionsPreviewFragmentContainer,Sessions_List);
+        ft.commit();
+
     }
 }
